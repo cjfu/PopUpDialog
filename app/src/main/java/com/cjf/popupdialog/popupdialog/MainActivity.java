5b44929744b688e8269c58c41e2a63d179d964b9
cjf <727783549@qq.com>
2019-06-17 16:12:44 +0800

    Demo完成

--- app/src/main/java/com/cjf/popupdialog/popupdialog/MainActivity.java
+++ app/src/main/java/com/cjf/popupdialog/popupdialog/MainActivity.java
@@ -2,12 +2,110 @@ package com.cjf.popupdialog.popupdialog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.cjf.popupdialog.utils.PopUpDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    PopUpDialog popUpDialog1;
+    PopUpDialog popUpDialog2;
+    PopUpDialog popUpDialog3;
+
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        //popupdialog初始化，决定了弹框的特性
+        popUpDialog1 = new PopUpDialog(R.id.main_layout,
+                R.layout.dialog_layout,
+                button1,
+                PopUpDialog.DIALOG_LEFT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                this);
+        popUpDialog2 = new PopUpDialog(R.id.main_layout,
+                R.layout.dialog_layout,
+                button2,
+                PopUpDialog.DIALOG_RIGHT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                this);
+        popUpDialog3 = new PopUpDialog(R.id.main_layout,
+                R.layout.dialog_layout,
+                button3,
+                PopUpDialog.DIALOG_UP,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                this);
+    }
+
+    /**
+     * 绑定事件
+     */
+    private void initListener() {
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+        popUpDialog1.setOnClickListener(R.id.dialog_button, this);//弹框中若只有一个控件需要绑定事件，可使用该方法绑定
+        int[] ids = {R.id.dialog_button};
+        popUpDialog2.setOnClickListener(ids, this);//弹框中若有多个控件需绑定事件，可使用该方法绑定
+        popUpDialog3.setOnClickListener(R.id.dialog_button, this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                if (popUpDialog1.isShowing()) {
+                    popUpDialog1.dismiss();
+                } else {
+                    popUpDialog1.showPopUpWindow();
+                }
+                break;
+            case R.id.button2:
+                if (popUpDialog2.isShowing()) {
+                    popUpDialog2.dismiss();
+                } else {
+                    popUpDialog2.showPopUpWindow();
+                }
+                break;
+            case R.id.button3:
+                if (popUpDialog3.isShowing()) {
+                    popUpDialog3.dismiss();
+                } else {
+                    popUpDialog3.showPopUpWindow();
+                }
+                break;
+            case R.id.dialog_button:
+                if (popUpDialog1.isShowing()) {
+                    popUpDialog1.dismiss();
+                }
+                if (popUpDialog2.isShowing()) {
+                    popUpDialog2.dismiss();
+                }
+                if (popUpDialog3.isShowing()) {
+                    popUpDialog3.dismiss();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/cjf/popupdialog/utils/PopUpDialog.java
+++ app/src/main/java/com/cjf/popupdialog/utils/PopUpDialog.java
@@ -12,7 +12,7 @@ import android.widget.PopupWindow;
 import com.cjf.popupdialog.popupdialog.R;
 
 /**
- * Created by cjf on 2017/8/14.
+ * Created by cjf.
  * 用于在任意位置弹出任意样式的弹框
  */
 
@@ -21,8 +21,6 @@ public class PopUpDialog {
     public final static int DIALOG_UP = 1;          //在依赖View的上方弹出
     public final static int DIALOG_LEFT = 2;        //在屏幕中间左侧弹出
     public final static int DIALOG_RIGHT = 3;       //在屏幕中间右侧弹出
-    public final static int DIALOG_VIEW_UP = 4;          //在依赖View的上方弹出
-
 
     private PopupWindow popupWindow;
     private View rootView;              //父布局View
@@ -35,18 +33,18 @@ public class PopUpDialog {
     /**
      * 用于构造一个弹框（点击控件后显示在该视图正上方）
      *
-     * @param rootViewId    activity的id，即activity跟节点的id。需要在xml中定义（R.id.xxx）
-     * @param dialogViewId  弹框布局文件id（R.layout.xxx）
-     * @param clickView     与弹框关联的控件（弹框将显示在该控件的上方）,若不为上弹框(DIALOG_UP)，则可传入null
-     * @param mode          弹出模式，用于决定打开方式。
-     * @param context       上下文
+     * @param rootViewId   activity的id，即activity跟节点的id。需要在xml中定义（R.id.xxx）
+     * @param dialogViewId 弹框布局文件id（R.layout.xxx）
+     * @param clickView    与弹框关联的控件（弹框将显示在该控件的上方）,若不为上弹框(DIALOG_UP)，则可传入null
+     * @param mode         弹出模式，用于决定打开方式。
+     * @param context      上下文
      */
     public PopUpDialog(int rootViewId, int dialogViewId, final View clickView, int mode, int Width, int Height, Activity context) {
-        if (context==null){
+        if (context == null) {
             return;
         }
         rootView = context.findViewById(rootViewId);
-        this.context=context;
+        this.context = context;
         this.clickView = clickView;
         this.mode = mode;
         popView = context.getLayoutInflater().inflate(dialogViewId, null);
@@ -61,8 +59,8 @@ public class PopUpDialog {
     }
 
     /**
-     * @param view  被定位的View
-     * @return  location[0]为x轴坐标，location[1]为y轴坐标
+     * @param view 被定位的View
+     * @return location[0]为x轴坐标，location[1]为y轴坐标
      */
     private int[] getViewLocation(View view) {
         int[] location = new int[2];
@@ -71,9 +69,8 @@ public class PopUpDialog {
     }
 
     /**
-     *
-     * @param popupWindow   被定位的View
-     * @return  location[0]为宽度，location[1]为高度
+     * @param popupWindow 被定位的View
+     * @return location[0]为宽度，location[1]为高度
      */
     private int[] getPopUpWindowViewSize(PopupWindow popupWindow) {
         int[] size = new int[2];
@@ -89,32 +86,34 @@ public class PopUpDialog {
 
     /**
      * 获取屏幕宽度
-     * @return  屏幕宽
+     *
+     * @return 屏幕宽
      */
-    private int getScreenWidth(){
+    private int getScreenWidth() {
         if (context != null) {
             DisplayMetrics dm = context.getResources().getDisplayMetrics();
             return dm.widthPixels;
-        }else{
+        } else {
             return 0;
         }
     }
 
     /**
      * 获取屏幕高度
-     * @return  屏幕高
+     *
+     * @return 屏幕高
      */
-    private int getScreenHeight(){
+    private int getScreenHeight() {
         if (context != null) {
             DisplayMetrics dm = context.getResources().getDisplayMetrics();
             return dm.heightPixels;
-        }else{
+        } else {
             return 0;
         }
     }
 
-    public void changeClickView(View view){
-        clickView=view;
+    public void changeClickView(View view) {
+        clickView = view;
     }
 
     public void showPopUpWindow() {
@@ -134,13 +133,13 @@ public class PopUpDialog {
                 popupWindow.setFocusable(false);
                 popupWindow.setAnimationStyle(R.style.PopupLeft);
                 popupWindow.showAtLocation(rootView, Gravity.NO_GRAVITY, 0,
-                        (getScreenHeight()-getPopUpWindowViewSize(popupWindow)[1])/2);
+                        (getScreenHeight() - getPopUpWindowViewSize(popupWindow)[1]) / 2);
                 break;
             case DIALOG_RIGHT:
                 popupWindow.setFocusable(false);
                 popupWindow.setAnimationStyle(R.style.PopupRight);
-                popupWindow.showAtLocation(rootView, Gravity.NO_GRAVITY, getScreenWidth()-getPopUpWindowViewSize(popupWindow)[0],
-                        (getScreenHeight()-getPopUpWindowViewSize(popupWindow)[1])/2);
+                popupWindow.showAtLocation(rootView, Gravity.NO_GRAVITY, getScreenWidth() - getPopUpWindowViewSize(popupWindow)[0],
+                        (getScreenHeight() - getPopUpWindowViewSize(popupWindow)[1]) / 2);
                 break;
             default:
                 break;
@@ -148,26 +147,26 @@ public class PopUpDialog {
         clickView.setSelected(true);
     }
 
-    public void dismiss(){
-        if(popupWindow.isShowing()) {
+    public void dismiss() {
+        if (popupWindow.isShowing()) {
             popupWindow.dismiss();
         }
     }
 
-    public boolean isShowing(){
+    public boolean isShowing() {
         return popupWindow.isShowing();
 
     }
 
-    public void setOnClickListener(int id, View.OnClickListener onClickListener){
-        if(popView!=null){
+    public void setOnClickListener(int id, View.OnClickListener onClickListener) {
+        if (popView != null) {
             View view = popView.findViewById(id);
             view.setOnClickListener(onClickListener);
         }
     }
 
-    public void setOnClickListener(int[] ids, View.OnClickListener onClickListener){
-        if(popView!=null){
+    public void setOnClickListener(int[] ids, View.OnClickListener onClickListener) {
+        if (popView != null) {
             for (int id : ids) {
                 View view = popView.findViewById(id);
                 view.setOnClickListener(onClickListener);
@@ -175,7 +174,7 @@ public class PopUpDialog {
         }
     }
 
-    public View getPopView(){
+    public View getPopView() {
         return popView;
     }
 }
